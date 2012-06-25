002a7e0cf0a1ee31d0c40748c9f954bd3c88b1ce
Arnav S <arn@home>
2012-06-24 22:54:40 -0500

    moved most of the scriptwork to workspace.js, tons of bugs as a result.

--- scripts/workspace.js
+++ scripts/workspace.js
@@ -50,7 +50,6 @@ $('.clear_canvas').click(function(){$('.stage').replaceWith('<div class="stage">
 
 // Load and Save Section
 
-/*
 function save_named_scripts(){
     var title = $('#script_name').val();
     var description = $('#script_description').val();
@@ -67,12 +66,13 @@ function save_named_scripts(){
             date: date,
             scripts: scripts_as_object()
         });
-        $("#save_block").dialog("close");
-    }else   
+        $("#save_dialog").dialog("close");
+    }
+	else
         alert("You must enter a name");
-}*/
+}
     
-function restore_from_export(){
+/*function restore_from_export(){
     reset_and_close_restore_dialog();
     $('#exp h2').html('Paste Exported Code below');
     $('#exp small').html('Paste Exported Code below');
@@ -96,26 +96,36 @@ function reset_and_close_restore_dialog(){
     $('#script_list').empty();
     $('#restore_dialog').bPopup().close();
 }
-
+*/
 function populate_and_show_restore_dialog(){
     var list = $('#script_list');
     var script_obj;
     var idx, value, key, script_li;
-    for (idx = 0; idx < localStorage.length; idx++){
+	var len = localStorage.length;
+    for (idx = 0; idx < len; idx++){
         key = localStorage.key(idx);
         if (key === '__current_scripts') continue;
         value = localStorage[key];
         script_obj = JSON.parse(value);
         if (script_obj.description){
-            script_li = $('<li><span class="title">' + script_obj.title + '</span><button class="restore action">Restore</button><button class="delete action">Delete</button><button class="show_description action">Description</button><br /><span class="timestamp">Saved on ' + new Date(script_obj.date).toDateString() + '</span><p class="description hidden">' + script_obj.description + '<p></li>');
+            script_li = $('<li><span class="title">' + script_obj.title + '</span><button class="restore action">Restore</button><button class="delete action">Delete</button><button class="show_description action">Description</button><br /><span class="timestamp">Saved on ' + new Date(script_obj.date).toDateString() + '</span><p class="description hidden">' + script_obj.description + '<br /><p></li>');
         }else{
             script_li = $('<li><span class="title">' + script_obj.title + '</span><button class="restore action">Restore</button><button class="delete action">Delete</button><br /><span class="timestamp">Saved on ' + new Date(script_obj.date).toDateString() + '</span></li>');
         }
+		
         script_li.data('scripts', script_obj.scripts); // avoid re-parsing later
         list.append(script_li);
     }
-    $('#restore_dialog').bPopup();
+	$('button.action').button();
+	$('#restore_dialog').dialog("open");
 }
+
+function save_current_scripts(){
+	show_workspace();
+	$('#accordion')[0].scrollIntoView();
+	localStorage['__current_scripts'] = JSON.stringify(scripts_as_object());
+}
+$(window).unload(save_current_scripts);
 
 var allDemos = {};
 
@@ -151,6 +161,7 @@ function restore_demo_by_name(name){
     clear_scripts();
     load_scripts_from_object(allDemos[name]);
 }
+
 window.restore_demo_by_name = restore_demo_by_name; // expose as public so we can link to a demo.
 
 function delete_named_scripts(event){
@@ -168,17 +179,30 @@ function toggle_description(event){
 
 $('.save_scripts').click(function(){$('#save_dialog').dialog("open");});
 
+/*
 $('.restore_scripts').click( populate_and_show_restore_dialog );
 $('#restore_dialog .cancel').click(reset_and_close_restore_dialog);
-$('#restore_dialog .exp').click(restore_from_export);
+$('#restore_dialog .exp').click(restore_from_export);*/
 $('#restore_dialog').delegate('.restore', 'click', restore_named_scripts)
                     .delegate('.show_description', 'click', toggle_description)
                     .delegate('.delete', 'click', delete_named_scripts);
-                    
+
+$('.restore_scripts').click(populate_and_show_restore_dialog);
+					
 $('#demos_dialog').delegate('.load', 'click', restore_demo_scripts)
                   .delegate('.show_description', 'click', toggle_description);
 $('#demos_dialog .cancel').click(function(){$('#demos_dialog').bPopup().close();});
 $('.demo_scripts').click(function(){$('#demos_dialog').bPopup();});
+
+function scripts_as_object(){
+	var blocks = $('.workspace:visible .scripts_workspace > .wrapper');
+	if (blocks.length){
+		return blocks.map(function(){return $(this).block_description();}).get();
+	}
+	else{
+		return [];
+	}   
+}
 
 function load_scripts_from_object(blocks){
     var workspace = $('.workspace:visible .scripts_workspace');
@@ -252,5 +276,141 @@ function menu(title, specs, show){
     return body;
 }
 window.menu = menu;
+				//Accordion menu on the right, now menus can be dragged around.
+				$("#accordion").accordion({
+					header: "> section > h3",
+					collapsible: true,
+					autoHeight: false
 
+				}).sortable({
+					axis: "y",
+					handle: "h3 > a",
+					stop: function( event, ui ) {
+						ui.item.children( "h3" ).triggerHandler( "focusout" );
+					}
+				});
+				
+				//Block/Text view
+				$( "#radio" ).buttonset();
+				
+				//Currently applies to ALL buttons on the stage, this may be an issue down the road.
+				$("button").button();
+				
+				//Save Dialog
+				var expsave = true;
+
+				
+				$("#save_dialog").dialog({
+					autoOpen: false,
+					height: 300,
+					width: 450,
+					modal: true,
+					resizable: false,
+					buttons: [
+					{
+						text: "Save",
+						"class": 'dialogbutton',
+						click: function() {save_named_scripts();}
+					},
+					{
+						text: "Export",
+						"class": 'dialogbutton',
+						click: function() {
+							var title = $('#script_name').val();    
+							var description = $('#script_description').val();
+							var date = Date.now();
+							if (title){
+								var exp = JSON.stringify({
+									title: title,
+									description: description,
+									date: date,
+									scripts: scripts_as_object()
+								});
+								$("#save_dialog").dialog("close");
+								$('#exp h2').html('Exported Code');
+								$('#exp small').html('Copy this into a text document to save for your records.');
+								expsave = true;
+								$('#exp').dialog("open");
+								$('#exp textarea').val(exp);
+							}
+							else
+							alert("You must enter a name");
+						}
+					},
+					{
+						text: "Cancel",
+						"class": 'dialogbutton',
+						click: function() {
+							$(this).dialog("close");
+						}
+					}
+					],
+					close: function(){
+						$("#script_name").val("");
+						$("#script_description").val("");
+					}
+				});
+				
+				//Restore Dialog
+				
+				$("#restore_dialog").dialog({
+					autoOpen: false,
+					height: 300,
+					width: 450,
+					modal: true,
+					resizable: false,
+					buttons: [
+					{
+						text: "Cancel",
+						"class": 'dialogbutton',
+						click: function() {
+							$(this).dialog("close");
+						}				
+					},
+					{
+						text: "Import",
+						"class": 'dialogbutton',
+						click: function() {
+							$(this).dialog("close");
+							$('#exp h2').html('Paste Exported Code below');
+							$('#exp small').html('Paste Exported Code below');
+							$('#exp textarea').val('');
+							expsave = false;
+							$('#exp').dialog("open");							
+						}
+					},
+					],
+					close: function(){
+						$('#script_list').empty();
+					}
+				});
+				
+				//Export Dialog				
+				$("#exp").dialog({
+					autoOpen: false,
+					height: 250,
+					width: 400,
+					modal: true,
+					resizable: false,
+					buttons: [
+					{
+						text: "Done",
+						"class": 'dialogbutton',
+						click: function() {
+							if(expsave){
+								$(this).dialog("close");
+							}
+							else{
+								var script = $('#exp textarea').val();
+								console.log(script);
+								$(this).dialog("close");
+								clear_scripts();
+								var ps = JSON.parse(script);
+								//console.log(ps.scripts);
+								load_scripts_from_object(ps.scripts);
+							}
+						}
+					}
+					]
+				});
 })(jQuery);
