245b586197144b2b70571b4f5b44fa678bdfddb7
ktey <k@tey>
2012-06-30 21:35:54 -0500

    Cleaning up some comments, and boolean fixing
    i tried to fix the problem with the boolean boxes, but no luck yet.

--- scripts/drag.js
+++ scripts/drag.js
@@ -81,7 +81,6 @@
 			case 'step': return step_targets();
             case 'container': return step_targets();
             case 'int': 
-			case 'boolean':
             case 'float': return socket_targets2(['any', 'number', drag_target.block_type()].join(','));
             case 'any': return socket_targets2('any');
             default: return socket_targets2(['any', drag_target.block_type()].join(','));
@@ -145,8 +144,20 @@
         // get position and append target to .content, adjust offsets
         // set last offset
         // TODO: handle detach better
-        if (drag_target.parent().is('.socket')){
-            drag_target.parent().append('<input />');
+        if (drag_target.parent().is('.socket')){////////look at this if statement
+			 /*var classes = drag_target.parent().attr('class');
+
+            classes = classes.replace("socket","").trim();
+            // console.log(classes);
+            if(classes == "boolean"){           
+                drag_target.parent().append('<select><option>true</option><option>false</option></select>');
+            }else{
+                if(!classes || classes=="string"){
+                    classes = '\"text\"';
+                }
+                drag_target.parent().append('<input type="'+classes+'"/>');
+            }*/
+			drag_target.parent().append('<input />');
         }
         drag_target.css('position', 'absolute');
 		if (drag_target.is('.scripts_workspace .wrapper')){
@@ -160,16 +171,13 @@
         drop_rects = $.map(potential_drop_targets, function(elem, idx){
             return $(elem).rect();
         });
-        // console.log('%s drop_rects', drop_rects.length);
-        // console.log('drop rects: %o', drop_rects);
 
         // start timer for drag events
         timer = setTimeout(hit_test, drag_timeout);
         return false;
     }
     
-    function drag(event){
-        // console.log('trying to drag, honestly');
+    function drag(event){ 
         if (!blend(event)) {return undefined;}
         if (!drag_target) {return undefined;}
         if (!current_position) {start_drag(event);}
