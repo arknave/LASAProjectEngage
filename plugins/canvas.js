96c9344962b0af5ca6a603ff957fbe4d9e1c3bbe
Arnav S <arn@home>
2012-07-22 21:09:22 -0500

    fixed stage detection

--- plugins/canvas.js
+++ plugins/canvas.js
@@ -73,7 +73,7 @@ jQuery.fn.extend({
   wrap_script: function(){
       // wrap the top-level script to prevent leaking into globals
       var script = this.pretty_script();
-      var retval = 'var global = new Global();console.log(global);(function($){var local = new Local();try{local.canvas = $("<canvas width=\\"" + global.stage_width + "\\" height=\\"" + global.stage_height + "\\"></canvas>").appendTo(".stage");local.ctx = local.canvas[0].getContext("2d");local.ctx.fillStyle="white";local.ctx.fillRect(0,0,200,200);' + script + '}catch(e){alert(e);}})(jQuery);';
+      var retval = 'var global = new Global();console.log(global);(function($){var local = new Local();try{local.canvas = $("<canvas width=\\"" + global.stage_width + "\\" height=\\"" + global.stage_height + "\\"></canvas>").appendTo(".stage");local.ctx = local.canvas[0].getContext("2d");local.ctx.fillStyle="white";local.ctx.fillRect(0,0,global.stage_width,global.stage_height);' + script + '}catch(e){alert(e);}})(jQuery);';
       //var retval = 'var global = new Global();(function($){var local = new Local();local.canvas = $("<canvas width=\\"" + global.stage_width + "\\" height=\\"" + global.stage_height + "\\"></canvas>").appendTo(".stage");local.ctx = local.canvas[0].getContext("2d");' + script + '})(jQuery);';
       return retval;
   },
@@ -105,7 +105,6 @@ function run_scripts(event){
 $('#run_scripts').click(run_scripts);
 
 // End UI section
-
 
 // expose these globally so the Block/Label methods can find them
 window.choice_lists = {
